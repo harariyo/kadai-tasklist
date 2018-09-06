f951340d1f2728bee5e64cba5ff75e4626130ef5
riyo.hara <harariyo355@gmail.com>
2018-09-06 05:30:07 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,10 +8,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">TaskList</a>
+                <a class="navbar-brand" href="/tasks/">TaskList</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Signup</a></li>
+                    <li><a href="#">Login</a></li>
                     <li>{!! link_to_route('tasks.create', '新規タスクの登録') !!}</li>
                 </ul>
             </div>
